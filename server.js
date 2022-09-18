e15497f5074bc07090fa96c2e710318e4b329d59
brada1604 <bagusnugroho104@gmail.com>
2022-09-18 23:55:17 +0700

    nambah kacida

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const {greeting} = require('./user')
 
-app.get('/hello', (req,res) => res.send('hello, wargi indonesia raya!'))
+app.get('/hello', (req,res) => res.send('hello, wargi indonesia raya kacida!'))
 // app.get('/', (req,res) => res.send('This App is running properly!'))
 // app.get('/ping', (req,res) => res.send('Pong!'))
 // app.get('/youtube', (req,res) => res.send('Hello, youtube indonesia!'))

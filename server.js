1d40ebe6c938cdb66ecf1ad427f59ea1fb444f46
brada1604 <bagusnugroho104@gmail.com>
2022-09-19 13:23:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const { greeting } = require('./user')
 
 app.get('/hello', (req, res) => res.send('hello, wargi indonesia raya kacida!'))
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/a', (req, res) => res.send('Hello World!'))
 // app.get('/ping', (req,res) => res.send('Pong!'))
 app.get('/youtube', (req, res) => res.send('Hello, youtube indonesia!'))
 // app.get('/hello/:name', (req,res) => {

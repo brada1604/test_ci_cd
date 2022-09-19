a136b9af31fc733e3730c9a6f719cde522773bfe
Muhamad Ardi Nur Insan <ardinurinsan03@gmail.com>
2022-09-19 15:31:12 +0700

    TEST : testinf

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const { greeting } = require('./user')
 
-app.get('/hello', (req, res) => res.send('hello, semuanya wargi indonesia raya kacida!'))
-app.get('/a', (req, res) => res.send('Hello World!'))
-app.get('/ping', (req,res) => res.send('Pong!'))
+app.get('/hello', (req, res) => res.send('hello, ini proyek 3'))
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/ping', (req, res) => res.send('Pong!'))
 app.get('/youtube', (req, res) => res.send('Hello, youtube indonesia!'))
 // app.get('/hello/:name', (req,res) => {
 //     res.json({message:greeting(req.params.name)})

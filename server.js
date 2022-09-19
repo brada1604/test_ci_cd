eb5339e94b373c137b3c7dce7d9bcb01f1ee043b
brada1604 <bagusnugroho104@gmail.com>
2022-09-19 14:54:39 +0700

    ping

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { greeting } = require('./user')
 
 app.get('/hello', (req, res) => res.send('hello, semuanya wargi indonesia raya kacida!'))
 app.get('/a', (req, res) => res.send('Hello World!'))
-// app.get('/ping', (req,res) => res.send('Pong!'))
+app.get('/ping', (req,res) => res.send('Pong!'))
 app.get('/youtube', (req, res) => res.send('Hello, youtube indonesia!'))
 // app.get('/hello/:name', (req,res) => {
 //     res.json({message:greeting(req.params.name)})
